8737df38cadb9fff4995b40b1cac0e18d481abf9
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-06-20 15:56:11 +0200

    Solve baby-steps

--- baby-steps.js
+++ baby-steps.js
@@ -1,8 +1,19 @@
 // Create funtion to add 2 numbers
-function sum (a, b) {
-    c = parseInt(a);
-    d = parseInt(b)
-    return c + d;
+// function sum (a, b) {
+//     c = parseInt(a);
+//     d = parseInt(b)
+//     return c + d;
+// }
+// console.log(sum(process.argv[2], process.argv[3]))
+
+// Create a function to add 1 or more numbers
+function sumVarious (...args) {
+    let result = 0;
+    for (let i = 2; i < args.length; i++) {
+        num = parseInt(args[i])
+        result += num;
+    }
+    return result;
 }
 
-console.log(sum(process.argv[2], process.argv[3]))
+console.log(sumVarious(...process.argv));
