d424a499e8204d4b2485fda6780cc35c25f35eec
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-06-26 14:06:52 +0200

    Start solving 9. juggling async

---
+++ juggling-async.js
@@ -0,0 +1,31 @@
+const http = require('node:http');
+let urls = [];
+let responses = [];
+var count = 0;
+
+for (let i = 2; i < process.argv.length; i++){
+    urls.push(process.argv[i]);
+}
+
+function getData(i) {
+    http.get(urls[i], (res) => {
+        res.setEncoding('utf-8');
+        res.on('data', (chunk) => { 
+            responses[i] += `${chunk}`; 
+        });
+        res.on('end', () => {
+            count++;
+            if (count == 3) {
+                responses.forEach(response => {
+                    console.log(response)
+                })
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    })
+}
+
+for (let i = 0; i < urls.length; i++) {
+    getData(i);
+}
